59d76271df722e4e8db49fc7dde094bded6a842b
Krzysztof Kalinski <krzysztof.kalinski@globallogic.com>
2023-01-24 10:43:05 +0100

    Menu

--- pacman.py
+++ pacman.py
@@ -1,18 +1,49 @@
 import pygame
+import sys
+mainClock = pygame.time.Clock()
+screen = pygame.display.set_mode((1200, 600))
+pygame.display.flip()
 
-pygame.init()
-WIDTH = 1200
-HEIGHT = 600
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-timer = pygame.time.Clock()
-fps = 60
-pygame.display.set_caption("Pac Man")
-run = True
-while run:
-    timer.tick(fps)
-    screen.fill("black")
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    pygame.display.flip()
-pygame.quit()
+click = False
+
+def main_menu():
+    while True:
+        screen.fill((0,0,0))
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        button1 = pygame.Rect(500, 100, 200, 50)
+        button2 = pygame.Rect(500, 200, 200, 50)
+        if button1.collidepoint((mouse_x, mouse_y)):
+            if click:
+                game()
+        if button2.collidepoint((mouse_x, mouse_y)):
+            if click:
+                pass
+        pygame.draw.rect(screen, (255, 255, 0), button1)
+        pygame.draw.rect(screen, (255, 255, 0), button2)
+        click = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+        pygame.display.update()
+        mainClock.tick(60)
+def game():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        pygame.display.update()
+        mainClock.tick(60)
+main_menu()
